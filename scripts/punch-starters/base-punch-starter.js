40442357b96aab82fc6ae3aa013dfb3e6b90d891
Yanislav-Asenov <yanislav.asenov@gmail.com>
2016-11-10 14:37:02 +0200

    Implemented BasePunchStarter class

--- scripts/punch-starters/base-punch-starter.js
+++ scripts/punch-starters/base-punch-starter.js
@@ -1,5 +1,49 @@
 class BasePunchStarter {
-    //TODO: Implement BasePunchStarter
+    constructor (id, name, manufacturer, description, genres, targetPrice) {
+        this._id = id;
+        this._name = name;
+        this._manufacturer = manufacturer;
+        this._description = description;
+        this._genres = genres;
+        this._targetPrice = targetPrice;
+        this.accumulatedMoney = 0;
+    }
+
+    get id () {
+        return this._id;
+    }
+
+    get name () {
+        return this._name;
+    }
+
+    get manufacturer () {
+        return this._manufacturer;
+    }
+
+    get description () {
+        return this._description;
+    }
+
+    get genres () {
+        return this._genres;
+    }
+
+    get targetPrice () {
+        return this._targetPrice;
+    }
+
+    set accumulatedMoney (value) {
+        if (tpyeof(value) !== 'number') {
+            throw new TypeError('Invalid accumulatedMoney: ' + value);
+        }
+        
+        this._accumulatedMoney = value;
+    }
+
+    get accumulatedMoney () {
+        return this._accumulatedMoney;
+    }
 }
 
 module.exports = BasePunchStarter;
