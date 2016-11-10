3e4bcecc4df3950180c85e82f283f08cfa3a4a60
achobanov <alexchobanov@gmail.com>
2016-11-10 15:42:16 +0200

    fixed CraftsPunchStarter

--- scripts/punch-starters/crafts-punch-starter.js
+++ scripts/punch-starters/crafts-punch-starter.js
@@ -1,33 +1,6 @@
-function validateProperties (id, name, manufacturer, description, genres, targetPrice, resources) {
-    if (typeof(id) !== 'number') {
-        throw new TypeError('Invalid id: '+ id);
-    }
+let BasePunchStarter = require("./base-punch-starter.js");
 
-    if (name !== 'string') {
-        throw new TypeError('Invalid name: '+ name);
-    }
-
-    if (manufacturer !== 'string') {
-        throw new TypeError('Invalid manufacturer: '+ manufacturer);
-    }
-
-    if (description !== 'string') {
-        throw new TypeError('Invalid description: '+ description);
-    }
-
-    if (!Array.isArray(genres)) {
-        throw new TypeError('Invalid genres: '+ genres);
-    }
-
-    for (let genre of genres) {
-        if (typeof(genre) !== 'string') {
-            throw new TypeError('Invalid genre: '+ genre);
-        }
-    }
-
-    if (typeof(targetPrice) !== 'number') {
-        throw new TypeError('Invalid targetPrice: '+ targetPrice);
-    }
+function validateProperties (resources) {
 
     if (!Array.isArray(resources)) {
         throw new TypeError(`Invalid resources. ${resources} is not an array.`);
@@ -40,10 +13,10 @@ function validateProperties (id, name, manufacturer, description, genres, target
     }
 }
 
-class CraftsPunchStarter {
+class CraftsPunchStarter extends BasePunchStarter{
     constructor(id, name, manufacturer, description, genres, targetPrice, resources) {
-        validateProperties(id, name, manufacturer, description, genres, targetPrice, resources);
         super(id, name, manufacturer, description, genres, targetPrice);
+        validateProperties(resources);
         this._resources = resources;
     }
 
