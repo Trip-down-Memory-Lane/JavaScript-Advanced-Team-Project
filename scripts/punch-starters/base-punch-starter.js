001ef2235d0c22fdfb0bcec65331bb112482e5a0
Yanislav-Asenov <yanislav.asenov@gmail.com>
2016-11-10 15:07:29 +0200

    Updated validation in BasePunchStarter class

--- scripts/punch-starters/base-punch-starter.js
+++ scripts/punch-starters/base-punch-starter.js
@@ -1,5 +1,42 @@
+function validateProperties (id, name, manufacturer, description, genres, targetPrice) {
+    if (typeof(id) !== 'number') {
+        throw new TypeError('Invalid id: '+ id);
+    }
+
+    if (name !== 'string') {
+        throw new TypeError('Invalid name: '+ name);
+    }
+
+    if (manufacturer !== 'string') {
+        throw new TypeError('Invalid manufacturer: '+ manufacturer);
+    }
+
+    if (description !== 'string') {
+        throw new TypeError('Invalid description: '+ description);
+    }
+
+    if (!Array.isArray(genres)) {
+        throw new TypeError('Invalid genres: '+ genres);
+    }
+
+    for (let genre of genres) {
+        if (typeof(genre) !== 'string') {
+            throw new TypeError('Invalid genre: '+ genre);
+        }
+    }
+
+    if (typeof(targetPrice) !== 'number') {
+        throw new TypeError('Invalid genre: '+ targetPrice);
+    }
+}
+
 class BasePunchStarter {
     constructor (id, name, manufacturer, description, genres, targetPrice) {
+        if (new.target === BasePunchStarter) {
+            throw new Error('Cannot create an instance of an abstract class');
+        }
+        validateProperties(id, name, manufacturer, description, genres, targetPrice);
+
         this._id = id;
         this._name = name;
         this._manufacturer = manufacturer;
@@ -34,10 +71,10 @@ class BasePunchStarter {
     }
 
     set accumulatedMoney (value) {
-        if (tpyeof(value) !== 'number') {
+        if (typeof(value) !== 'number') {
             throw new TypeError('Invalid accumulatedMoney: ' + value);
         }
-        
+
         this._accumulatedMoney = value;
     }
 
