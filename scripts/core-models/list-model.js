bdf5a23421a896e62de6e35543435967a2a97869
Asya Dikova <asya.iv.dikova@gmail.com>
2016-11-10 17:07:38 +0200

    List Model

--- scripts/core-models/list-model.js
+++ scripts/core-models/list-model.js
@@ -1,5 +1,22 @@
 class ListModel {
-	//TODO: Implement List Model
+    constructor(){
+    }
+
+    render(database) {
+        database.sort((a,b) => a.id - b.id);
+
+        let html = '<div class="punch-starter-list-holder">' +
+            '   <table class="punch-starter-table">'+
+            `<tr><th>ID</th><th>Name</th><th>Manufacturer</th><th>Type</th><th>Progress</th></tr>`;
+        for(let element of database){
+            let typeOfElement = element.constructor.name.replace("PunchStarter", "");
+            html += `<tr><td>${element.id}</td><td>${element.name}</td><td>${element.manufacturer}</td><td>${typeOfElement}</td><td>${element.accumulatedMoney}/${element.targetPrice}</td></tr>`;
+        }
+
+        html+= '    </table>' +
+            '</div>';
+        $('.wrapper main').html(html);
+    }
 }
 
 module.exports = ListModel;
