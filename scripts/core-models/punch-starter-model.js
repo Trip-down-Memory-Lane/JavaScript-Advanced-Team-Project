6a4b13a634883329fd1074b184e4b5db7043de90
achobanov <alexchobanov@gmail.com>
2016-11-11 20:28:04 +0200

    Finished renderLists()

--- scripts/core-models/punch-starter-model.js
+++ scripts/core-models/punch-starter-model.js
@@ -26,7 +26,69 @@ class PunchStarterModel {
 
     renderLists(punchStarter) {
         let html =
-            ``
+            `<div>` +
+                `<label>Genres</label>` +
+                `<ul>`;
+        punchStarter._genres.forEach(x => html += `<li>${x}</li>`);
+        html +=
+                `</ul>` +
+            `</div>`;
+        switch (punchStarter.constructor.name) {
+            case`MoviePunchStarter`:
+                html +=
+                    `<div>` +
+                        `<label>Actors</label>` +
+                        `<ul>`;
+                punchStarter._actors.forEach(x => html += `<li>${x}</li>`);
+                html +=
+                        `</ul>` +
+                    `</div>`;
+                html +=
+                    `<div>` +
+                        `<label>Director</label>` +
+                        `<p>${punchStarter._director}</p>` +
+                    `</div>`;
+                break;
+            case`GamePunchStarter`:
+                html +=
+                    `<div>` +
+                        `<lable>Technologies Used</lable>` +
+                        `<ul>`;
+                punchStarter._technologiesUsed.forEach(x => html += `<li>${x}</li>`);
+                html +=
+                        `</ul>` +
+                    `</div>`;
+                break;
+            case`InnovativePunchStarter`:
+                break;
+            case`FoodPunchStarter`:
+                html +=
+                    `<div>` +
+                        `<label>Ingredients</label>` +
+                        `<ul>`;
+                punchStarter._ingredients.forEach(x => html += `<li>${x}</li>`);
+                html +=
+                        `</ul>` +
+                    `</div>`;
+                html +=
+                    `<div>` +
+                        `<label>Recipe</label>` +
+                        `<p>${punchStarter._recipe}</p>` +
+                    `</div>`;
+                break;
+            case`CraftsPunchStarter`:
+                html +=
+                    `<div>` +
+                        `<label>Resources</label>` +
+                        `<ul>`;
+                punchStarter._resources.forEach(x => html += `<li>${x}</li>`);
+                html +=
+                        `</ul>` +
+                    `</div>`;
+                break;
+        }
+
+        $(`.punch-starter-lists`).append(html);
     }
 
     renderProgress(punchStarter) {
